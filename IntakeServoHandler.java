010e47838b3801c61e522fd5f66a818ca53e7285
Fang Hai Sen <hansonfang1012@gmail.com>
2024-12-28 12:52:20 -0800

    Create IntakeServoHandler.java

---
+++ IntakeServoHandler.java
@@ -0,0 +1,46 @@
+//FILE: IntakeServoHandler
+//resources
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class IntakeServoHandler {
+    // grab servo
+    Servo grabServo;
+    int grabbing;
+
+    // initialization
+    public IntakeServoHandler(HardwareMap hardwareMap) {
+        // set grab servo
+        intakeServo = hardwareMap.get(Servo.class, "grabServo");
+
+        // set initial state
+        grabbing    = 0;
+    }
+    public void loop(Gamepad gamepad1) {
+        //intake
+        boolean buttonA = gamepad1.a;
+        //Outake
+        boolean buttonB = gamepad1.b;
+        //running servo
+        if(buttonA && !buttonB){
+            grabbing = 1;
+        }
+        else if(!buttonA && buttonB){
+            grabbing = 2;
+        }
+        else{
+            //useless
+            grabbing = 0;
+        }
+        
+        if(grabbing == 1){
+            grabServo.setPosition(0);
+        }
+        if(grabbing == 2){
+            grabServo.setPosition(1);
+        }
+    }
+}
