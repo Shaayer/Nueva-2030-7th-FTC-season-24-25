d24be3c3122b9751273ad7cecd6ce7e82367aaf9
Fang Hai Sen <hansonfang1012@gmail.com>
2024-12-28 12:50:48 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,46 @@
+//FILE: Main.java
+
+//MAIN
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+// main op mode
+@TeleOp
+public class Main extends OpMode {
+    // handlers declared
+    DrivingHandler drivingHandler;
+    TowerServoHandler  towerServoHandler;
+    ClawServoHandler claweMotorHandler;
+    IntakeServoHandler intakeServoHandler;
+    ElbowHandler elbowHandler;
+
+    // initialization
+    public void init() {
+        // setup handlers
+        drivingHandler = new DrivingHandler(hardwareMap);
+        towerMotorHandler  = new TowerMotorHandler(hardwareMap);
+        clawServoHandler  = new ClawServoHandler(hardwareMap);
+        intakeServoHandler  = new IntakeServoHandler(hardwareMap);
+        elbowHandler = new ElbowHandler(hardwareMap);
+
+        telemetry.addData("status", "initialized");
+        telemetry.update();
+    }
+
+    // gameplay loop
+    public void loop() {
+        // run handlers
+        drivingHandler.loop(gamepad1);
+        towerMotorHandler.loop(gamepad1);
+        clawServoHandler.loop(gamepad1);
+        intakeServoHandler.loop(gamepad1);
+        elbowHandler.loop(gamepad1);
+        //grabServoHandler.loop(gamepad1);
+        //slideMotorHandler.loop(gamepad1);
+
+        telemetry.addData("status", "running");
+        telemetry.update();
+    }
+}
