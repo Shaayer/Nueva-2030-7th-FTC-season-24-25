e84e85607545dcab7b26ac744e837508545e2ec6
Andqiu1 <andreqiu99@gmail.com>
2025-01-27 17:01:37 -0800

    Update Vision.java

--- Vision.java
+++ Vision.java
@@ -1,27 +1,27 @@
-//note: This is not on OnBotJava yet.
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.opencv.core.Size;
 
 @TeleOp
 public class Vision extends LinearOpMode {
 
     @Override
-    public void runOpMode throws InterruptedException {
-        
+    public void runOpMode() throws InterruptedException {
+
         AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
             .setDrawAxes(true)
             .setDrawCubeProjection(true)
             .setDrawTagID(true)
             .setDrawTagOutline(true)
             .build();
-        
+
+        // Initialize the Vision Portal
         VisionPortal visionPortal = new VisionPortal.Builder()
             .addProcessor(tagProcessor)
             .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
@@ -30,17 +30,24 @@ public class Vision extends LinearOpMode {
 
         waitForStart();
 
-        while (!isStopRequested() && opModeIsActive){
-            if(tagProcessor.getDetections.size() > 0){
+        while (opModeIsActive() && !isStopRequested()) {
+
+            if (!tagProcessor.getDetections().isEmpty()) {
                 AprilTagDetection tag = tagProcessor.getDetections().get(0);
 
+                // Add telemetry data for pose
                 telemetry.addData("x", tag.ftcPose.x);
                 telemetry.addData("y", tag.ftcPose.y);
                 telemetry.addData("z", tag.ftcPose.z);
                 telemetry.addData("pitch", tag.ftcPose.pitch);
                 telemetry.addData("roll", tag.ftcPose.roll);
                 telemetry.addData("yaw", tag.ftcPose.yaw);
+            } else {
+                telemetry.addData("Status", "No AprilTags detected");
             }
+
+            telemetry.update();
         }
+        //visionPortal.close();
     }
 }
