d0eb6131e52290d95f9a9b326c263ab37f45df52
Fang Hai Sen <hansonfang1012@gmail.com>
2025-02-06 18:23:47 -0800

    Update ClawServoHandler.java

--- ClawServoHandler.java
+++ ClawServoHandler.java
@@ -1,40 +1,46 @@
+//Team Code
 package org.firstinspires.ftc.teamcode;
-
+//Import Hardware Map
 import com.qualcomm.robotcore.hardware.HardwareMap;
+//Import Gampad features
 import com.qualcomm.robotcore.hardware.Gamepad;
+//Import Servo Features
 import com.qualcomm.robotcore.hardware.Servo;
 
-// grab servo handler
+//Claw Servo Class
 public class ClawServoHandler {
-    // grab servo
+    //Servo Initialization
     Servo grabServo;
+    //If grabbing or not
     boolean grabbing;
-
-    // initialization
+    boolean preGrabbing;
+    double angle = 0.2;
+    //ClawServoHandler Class Constructor for Initialization
     public ClawServoHandler(HardwareMap hardwareMap) {
-        // set grab servo
+        //Servo class variable to use servos
         grabServo = hardwareMap.get(Servo.class, "grabServo");
-
-        // set initial state
+        //Set Initial State of claw
         grabbing    = false;
-        
+        preGrabbing    = false;
     }
+    
+    //Loop for commands
     public void loop(Gamepad gamepad1) {
-        float leftTrigger = gamepad1.left_trigger;
-        float rightTrigger = gamepad1.right_trigger;
-
-        if(rightTrigger>=0.5){
-            grabbing = false;
-        }
-        if(leftTrigger>=0.5){
-            grabbing = true;
-        }
-        
-        // set servo position
-        if(grabbing){
-            grabServo.setPosition(0.65);
-        }else{
-            grabServo.setPosition(0.8);
-        }
+       boolean buttonY = gamepad1.x;
+            if(buttonY){
+                if(preGrabbing){
+                    grabbing = !grabbing;
+                }
+                preGrabbing = false;
+            }else{
+                preGrabbing = true;
+            }
+            if(grabbing){
+                angle=0.42;
+            }
+            if(!grabbing){
+                angle=0.25;
+            }
+        grabServo.setPosition(angle);
     }
 }
